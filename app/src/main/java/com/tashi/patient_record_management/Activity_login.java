cf72c35387ecd609e0837f5453972b6d2e4459dc
tashi <arg.tashi@gmail.com>
2020-01-29 12:15:25 +0545

    login and registration page was designed completed

--- app/src/main/java/com/tashi/patient_record_management/Activity_login.java
+++ app/src/main/java/com/tashi/patient_record_management/Activity_login.java
@@ -2,13 +2,31 @@ package com.tashi.patient_record_management;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class Activity_login extends AppCompatActivity {
+public class Activity_login extends AppCompatActivity implements View.OnClickListener {
+    TextView tv_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tv_register = findViewById(R.id.tv_register);
+        tv_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(Activity_login.this, Activity_register.class);
+                startActivity(registerIntent);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
