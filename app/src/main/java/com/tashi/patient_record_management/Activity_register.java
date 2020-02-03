ffa3128a01ed204923c2baf3043a838310cb278c
tashi <arg.tashi@gmail.com>
2020-02-03 20:35:31 +0545

    Changed some UI design in dashboard

--- app/src/main/java/com/tashi/patient_record_management/Activity_register.java
+++ app/src/main/java/com/tashi/patient_record_management/Activity_register.java
@@ -1,15 +1,27 @@
 package com.tashi.patient_record_management;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity_register extends AppCompatActivity {
+    TextView tvLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        tvLogin = findViewById(R.id.tv_login);
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Activity_login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
