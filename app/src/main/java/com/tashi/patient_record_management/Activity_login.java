430d71950b2caf817868ee00d9bc2ce8eb641240
tashi <arg.tashi@gmail.com>
2020-02-03 23:29:08 +0545

    Log in and registration was connected to firebase. Authentication was also done

--- app/src/main/java/com/tashi/patient_record_management/Activity_login.java
+++ app/src/main/java/com/tashi/patient_record_management/Activity_login.java
@@ -1,32 +1,87 @@
 package com.tashi.patient_record_management;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Activity_login extends AppCompatActivity implements View.OnClickListener {
-    TextView tv_register;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Activity_login extends AppCompatActivity {
+    private  TextView txtsignup, btnsignin, login_username , login_password,button_signin;
+    FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        firebaseAuth = FirebaseAuth.getInstance();
+        login_password = findViewById(R.id.pswd);
+        login_username = findViewById(R.id.user);
+        button_signin = findViewById(R.id.lin);
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(Activity_login.this, "Login successful", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(Activity_login.this, Dashboard.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(Activity_login.this, "Login to continue", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
 
-        tv_register = findViewById(R.id.tv_register);
-        tv_register.setOnClickListener(new View.OnClickListener() {
+        txtsignup = findViewById(R.id.tv_register);
+        txtsignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent registerIntent = new Intent(Activity_login.this, Activity_register.class);
-                startActivity(registerIntent);
+                Intent i = new Intent(Activity_login.this, Activity_register.class);
+                startActivity(i);
+            }
+        });
+        button_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userEmail = login_username.getText().toString();
+                String userPaswd = login_password.getText().toString();
+                if (userEmail.isEmpty()) {
+                    login_username.setError("Check your Email!");
+                    login_username.requestFocus();
+                } else if (userPaswd.isEmpty()) {
+                    login_password.setError("Enter Password!");
+                    login_password.requestFocus();
+                } else if (userEmail.isEmpty() && userPaswd.isEmpty()) {
+                    Toast.makeText(Activity_login.this, "Fields Empty!", Toast.LENGTH_SHORT).show();
+                } else if (!(userEmail.isEmpty() && userPaswd.isEmpty())) {
+                    firebaseAuth.signInWithEmailAndPassword(userEmail, userPaswd).addOnCompleteListener(Activity_login.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(Activity_login.this, "Not successful", Toast.LENGTH_SHORT).show();
+                            } else {
+                                startActivity(new Intent(Activity_login.this, Dashboard.class));
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(Activity_login.this, "Error", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
-
     @Override
-    public void onClick(View v) {
-
+    protected void onStart(){
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
     }
 }
--- app/src/main/java/com/tashi/patient_record_management/Activity_register.java
+++ app/src/main/java/com/tashi/patient_record_management/Activity_register.java
@@ -1,27 +1,73 @@
 package com.tashi.patient_record_management;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Activity_register extends AppCompatActivity {
-    TextView tvLogin;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class Activity_register extends AppCompatActivity {
+    private TextView txtsignin, txttas;
+    private TextView username, email, password, con_password, btnsignup;
+
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        tvLogin = findViewById(R.id.tv_login);
-        tvLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Activity_login.class);
-                startActivity(intent);
+        firebaseAuth = FirebaseAuth.getInstance();
+        username = findViewById(R.id.user);
+        email = findViewById(R.id.mail);
+        password = findViewById(R.id.pswd);
+        con_password = findViewById(R.id.mobphone);
+        btnsignup = findViewById(R.id.tv_login);
+        btnsignup.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View view){
+                String emailID = email.getText().toString();
+                String paswd = password.getText().toString();
+                String conpaswd = con_password.getText().toString();
+                String user_name = username.getText().toString();
+
+                if (emailID.isEmpty()) {
+                    email.setError("Check your email");
+                    email.requestFocus();
+                } else if (paswd.isEmpty()) {
+                    password.setError("Set your password");
+                    password.requestFocus();
+                } else if (conpaswd.isEmpty()) {
+                    con_password.setError("Set the mobile number");
+                    con_password.requestFocus();
+                } else if (user_name.isEmpty()) {
+                    username.setError("Set a username");
+                    username.requestFocus();
+                } else if (emailID.isEmpty() && paswd.isEmpty() && user_name.isEmpty() && conpaswd.isEmpty()) {
+                    Toast.makeText(Activity_register.this, "Please fill everything!", Toast.LENGTH_SHORT).show();
+                } else if (!(emailID.isEmpty() && paswd.isEmpty() && user_name.isEmpty() && conpaswd.isEmpty())) {
+                    firebaseAuth.createUserWithEmailAndPassword(emailID, paswd).addOnCompleteListener(Activity_register.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(Activity_register.this.getApplicationContext(), "Signup unsuccesfull:" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            } else {
+                                startActivity(new Intent(Activity_register.this, Activity_login.class));
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(Activity_register.this, "Error", Toast.LENGTH_SHORT).show();
+                }
             }
+
+
         });
+
     }
 }
