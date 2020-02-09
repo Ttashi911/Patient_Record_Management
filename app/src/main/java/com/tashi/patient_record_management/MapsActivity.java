ce1c02d7d96e23e4f6c93ec4975dcf022b2b3276
tashi <arg.tashi@gmail.com>
2020-02-09 23:47:20 +0545

    Location was fixed in different desired places

--- app/src/main/java/com/tashi/patient_record_management/MapsActivity.java
+++ app/src/main/java/com/tashi/patient_record_management/MapsActivity.java
@@ -55,6 +55,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng defaultmapview= new LatLng(27.706253, 85.330643);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(defaultmapview));
 
+        LatLng boudha = new LatLng(27.720757,85.360949);
+        MarkerOptions markerOptions1 = new MarkerOptions().position(boudha).title("Stupa Hospital in boudha pipolbot").snippet("").icon(BitmapDescriptorFactory.fromBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.hospital)));
+        mMap.addMarker(markerOptions1);
+
+        LatLng chabel = new LatLng(27.717083,85.346733);
+        MarkerOptions markerOptions2 = new MarkerOptions().position(chabel).title("Medicare Hospital in chabel").snippet("").icon(BitmapDescriptorFactory.fromBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.hospital)));
+        mMap.addMarker(markerOptions2);
+
+        LatLng gaushala = new LatLng(27.7097063,85.34640941);
+        MarkerOptions markerOptions3 = new MarkerOptions().position(gaushala).title("Metro Hospital in gaushala").snippet("").icon(BitmapDescriptorFactory.fromBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.hospital)));
+        mMap.addMarker(markerOptions3);
+
+        LatLng gairidhara = new LatLng(27.7177283,85.3278327);
+        MarkerOptions markerOptions4 = new MarkerOptions().position(gairidhara).title("Beeraj Baal Hospital in gairidhara").snippet("").icon(BitmapDescriptorFactory.fromBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.hospital)));
+        mMap.addMarker(markerOptions4);
+
 
 
     }
