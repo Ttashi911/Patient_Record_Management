ede4072c1d97f0b6c140c64b457456fcd8dad83e
tashi <arg.tashi@gmail.com>
2020-02-12 00:24:53 +0545

    Feedback was completed and saved in database

---
+++ app/src/main/java/com/tashi/patient_record_management/Activity_appointment.java
@@ -0,0 +1,14 @@
+package com.tashi.patient_record_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_appointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment);
+    }
+}
